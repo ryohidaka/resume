72de228f5bd6ed88db27adf4e3a20e87c29e54d9
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-05-02 20:53:09 +0900

    feat: 職務経歴データを追加

--- docs/.vitepress/data/project.data.ts
+++ docs/.vitepress/data/project.data.ts
@@ -4,15 +4,35 @@ export default {
   load() {
     return [
       {
-        title: "",
-        startAt: "",
+        title: "映像配信サービス",
+        startAt: "2023-03",
         info: {
-          summary: "",
-          phase: [],
-          jobDescription: [],
+          summary: "大手映像配信サービスのWEBサイトの開発",
+          phase: ["設計", "開発", "テスト", "運用保守"],
+          jobDescription: [
+            "追加機能の設計・実装",
+            "不具合修正",
+            "最適化を含むリファクタリング対応",
+          ],
         },
-        environment: { language: [], framework: [] },
-        scale: { count: 0, role: "エンジニア" },
+        environment: { language: ["TypeScript"], framework: ["Next.JS"] },
+        scale: { count: 10, role: "エンジニア" },
+      },
+      {
+        title: "映像配信サービス",
+        startAt: "2023-06",
+        endAt: "2024-03",
+        info: {
+          summary: "大手映像配信サービスのiOSアプリケーションの開発",
+          phase: ["設計", "開発", "テスト", "運用保守"],
+          jobDescription: [
+            "追加機能の設計・実装",
+            "不具合修正",
+            "最適化を含むリファクタリング対応",
+          ],
+        },
+        environment: { language: ["Swift"], framework: ["UIKit"] },
+        scale: { count: 10, role: "エンジニア" },
       },
     ] as ProjectType[];
   },
