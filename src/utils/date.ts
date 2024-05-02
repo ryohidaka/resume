d8c3df32df82b6dbf984b466bcf8cdb323edda11
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-05-02 17:20:34 +0900

    feat: 現在の年齢を取得する処理を追加

---
+++ src/utils/date.ts
@@ -0,0 +1,38 @@
+// 現在の日付を取得する。
+const currentDate = new Date();
+
+/**
+ * 日付を日本時間の日付整数に変換する。
+ * @param {Date} date - 変換する日付
+ * @return {number} 日本時間の日付整数
+ */
+const toJapaneseDateInt = (date: Date): number => {
+  const dateString = date
+    .toLocaleDateString("sv-SE", { timeZone: "Asia/Tokyo" })
+    .replace(/[^0-9]/g, "");
+  return parseInt(dateString, 10);
+};
+
+/**
+ * 年齢を取得する。
+ * @return {number} 年齢
+ */
+const getAge = (): number => {
+  // 生年月日を設定する。
+  const year = 1997;
+  const month = 12;
+  const day = 19;
+
+  // 生年月日をDateオブジェクトに変換し、日本時間の日付整数に変換する。
+  const birthday = toJapaneseDateInt(new Date(year, month - 1, day));
+
+  // 現在の日付を日本時間の日付整数に変換する。
+  const today = toJapaneseDateInt(currentDate);
+
+  // 年齢を計算する。
+  const age = Math.floor((today - birthday) / 10000);
+
+  return age;
+};
+
+export const AGE = getAge();
